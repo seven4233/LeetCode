eb3ce0f1612b755465d157383b8ea272e0e50b5e
seven4233 <1985858775@qq.com>
2022-07-24 10:40:54 +0800

    (中等)最接近的三数之和

--- 16.threeSumClosest.js
+++ 16.threeSumClosest.js
@@ -1,19 +1,24 @@
-/* 
-    中等  --最接近的三数之和 
-    
-*/
+/*
+ * @Author: seven4233 1985858775@qq.com
+ * @Date: 2022-07-11 10:22:25
+ * @LastEditors: seven4233 1985858775@qq.com
+ * @LastEditTime: 2022-07-24 10:40:32
+ * @FilePath: \LeetCode\16.threeSumClosest.js
+ * @Description: (中等)  --最接近的三数之和 
+ */
 
-var threeSumClosest = function (nums, target) {
+var threeSumClosest = function (nums, target)  {
   let ans = [];
   nums.sort((a, b) => a - b);
 
   for (let i = 0; i < nums.length - 1; i++) {
-    let left = i + 1;
+    let left = i + 1; 
     let right = nums.length - 1;
     while (left < right) {
-      let res = nums[i] + nums[left] + nums[right];
+      let res = nums[i] + nums[left] + nums[right]; //三数之和
       res >= target
-        ? ((ans[res - target] = res), right--)
+      // res-target 的值越小，表示约接近,即在数组中的位置越靠前
+        ? ((ans[res - target] = res), right--) //res大于等于target, 右指针左移
         : ((ans[target - res] = res), left++);
     }
   }
