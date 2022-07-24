63c2086a32b3aaa88c74158484a14f7ff28bccea
seven4233 <1985858775@qq.com>
2022-07-24 10:19:01 +0800

    (中等)盛水最多的容器

--- 11.maxArea.js
+++ 11.maxArea.js
@@ -1,5 +1,13 @@
+/*
+ * @Author: seven4233 1985858775@qq.com
+ * @Date: 2022-07-10 09:05:35
+ * @LastEditors: seven4233 1985858775@qq.com
+ * @LastEditTime: 2022-07-24 09:46:55
+ * @FilePath: \LeetCode\11.maxArea.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 //  中等 盛水最多的容器
-
+ 
 /* 
     --采用双指针法， 从两端开始遍历， 只移动较短的柱子
     
